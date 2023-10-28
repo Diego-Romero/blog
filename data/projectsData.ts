0af4cff59517ec02b236a37e9b65cc19fe8b84ae
Diego Romero <dev.diego.romero@gmail.com>
2023-10-29 01:00:53 +0200

    feat: added projects page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: "A Search Engine",
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: "/static/images/google.png",
-    href: "https://www.google.com",
+    title: "Deepflow - 2021 - discontinued",
+    description: "What if you could use a single platform for to help you focus on your work, manage your tasks, and track the time you've been productive?",
+    imgSrc: "/static/images/focus.png",
+    href: "https://deepflow.vercel.app/",
   },
   {
-    title: "The Time Machine",
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: "/static/images/time-machine.jpg",
-    href: "/blog/the-time-machine",
+    title: "Kodr - 2018 - discontinued",
+    description: "Technical interview preparation platform for developers in Spanish",
+    imgSrc: "/static/images/coding.png",
+    href: "https://deepflow.vercel.app/",
   },
 ]
 
