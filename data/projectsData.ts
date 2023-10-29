a555289a46b0ac91bbbbca5e5ab0921c47da8482
Diego Romero <dev.diego.romero@gmail.com>
2023-10-29 16:44:45 +0100

    feat: refactored images to be jpg and added blogs for nextjs types of rendering and pomodoro technique

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: "Deepflow - 2021 - discontinued",
     description: "What if you could use a single platform for to help you focus on your work, manage your tasks, and track the time you've been productive?",
-    imgSrc: "/static/images/focus.png",
+    imgSrc: "/static/images/projects/focus.jpg",
     href: "https://deepflow.vercel.app/",
   },
   {
     title: "Kodr - 2018 - discontinued",
     description: "Technical interview preparation platform for developers in Spanish",
-    imgSrc: "/static/images/coding.png",
+    imgSrc: "/static/images/projects/coding.jpg",
     href: "https://deepflow.vercel.app/",
   },
 ]
