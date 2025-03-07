c8f36534ee37596bbfd7c8a1674d3537511156aa
Diego Romero <dev.diego.romero@gmail.com>
2025-03-07 11:56:27 -0300

    feat: Update project data and book review with enhanced content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,16 +1,28 @@
 const projectsData = [
   {
-    title: "Deepflow - 2021 - discontinued",
-    description:
-      "What if you could use a single platform for to help you focus on your work, manage your tasks, and track the time you've been productive?",
+    title: "Deepflow",
+    description: `
+    Deepflow is redefining the way we interact with technology by tackling one of the biggest crises of our time: the broken attention economy. In a world where people are constantly distracted, overwhelmed, and trapped in dopamine-driven digital loops, Deepflow helps individuals reclaim their focus, work deeply, and unlock their full potential.
+
+Our platform is designed for knowledge workers who want to break free from digital noise, structure their time more effectively, and achieve peak performance without burnout. We do this by integrating AI-driven productivity coaching, distraction-blocking mechanisms, and behavioral insights to create a seamless system that promotes deep work and intentional living.
+
+Unlike traditional productivity tools, Deepflow doesn’t just help you organize tasks—it helps you fundamentally change the way you work and live.
+    `,
     imgSrc: "/static/images/projects/focus.jpg",
-    href: "https://deepflow.vercel.app/",
+    href: "https://deepflow.me",
   },
   {
     title: "Kodr - 2018 - discontinued",
     description: "Technical interview preparation platform for developers in Spanish",
     imgSrc: "/static/images/projects/coding.jpg",
     href: "https://deepflow.vercel.app/",
+  },
+  {
+    title: "Listu - 2021 - discontinued",
+    description:
+      "Listu is a platform that allows you to create and manage your own lists and share it with other people.",
+    imgSrc: "/static/images/projects/coding.jpg",
+    href: "https://listu.me",
   },
 ]
 
